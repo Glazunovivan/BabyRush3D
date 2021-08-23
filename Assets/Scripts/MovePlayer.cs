280e0aadbaabfebe787c804009324c95ccf89a8c
Ivan <ivan.glaz.2029@mail.ru>
2021-08-23 21:32:50 +0300

    Add project

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,62 @@
+﻿using PathCreation;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class MovePlayer : MonoBehaviour
+{
+    private PathCreator _pathCreator;
+    private float _distanceTravelled;
+
+    [SerializeField] private float _speed;
+    [SerializeField] private bool _isRun = false;
+
+    //для движения с rigidbody
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponentInChildren<Rigidbody>();
+        //_rigidbody = GetComponent<Rigidbody>();
+        _pathCreator = FindObjectOfType<PathCreator>();
+        transform.rotation = _pathCreator.path.GetRotationAtDistance(_distanceTravelled);
+        transform.position = new Vector3(_pathCreator.path.GetPointAtDistance(_distanceTravelled).x,
+                                         _pathCreator.path.GetPointAtDistance(_distanceTravelled).y,
+                                         _pathCreator.path.GetPointAtDistance(_distanceTravelled).z);
+    }
+
+    private void Update()
+    {
+        if (_isRun)
+        {
+            RunPlayer();
+        }
+    }
+
+    private void RunPlayer()
+    {
+        TransformPositionPlayer();
+    }
+    private void TransformPositionPlayer()
+    {
+        _distanceTravelled +=  _speed * Time.deltaTime;
+        transform.rotation = _pathCreator.path.GetRotationAtDistance(_distanceTravelled);
+        transform.position = new Vector3(_pathCreator.path.GetPointAtDistance(_distanceTravelled).x,
+                                         _pathCreator.path.GetPointAtDistance(_distanceTravelled).y,
+                                         _pathCreator.path.GetPointAtDistance(_distanceTravelled).z);
+    }
+
+    public void StartRun()
+    {
+        _isRun = true;
+    }
+    public void StopRun()
+    {
+        _rigidbody.velocity = new Vector3(0,0,0);
+        _isRun = false;
+    }
+    public void StopRunIntoObstacle()
+    {
+        _isRun = false;
+    }
+
+}
---
+++ Assets/Scripts/Slide.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Slide : MonoBehaviour
+{
+    //private FloatingJoystick _joystick;
+
+    //чтобы не выходить за границы
+    private float _offsetSlide = 0.6f;
+    private float _currentPositionX = 0;
+    private float _speedSlide = 7;
+    private float difference = 0;
+
+    private float? lastMousePoint = null;
+
+    private void Start()
+    {
+        //_joystick = FindObjectOfType<FloatingJoystick>();
+    }
+    private void Update()
+    {
+        //if (_joystick.Horizontal != 0)
+        //{
+        //    _currentPositionX = _joystick.Horizontal * _offsetSlide;
+        //    Debug.Log(_currentPositionX);
+        //}
+        //transform.localPosition = new Vector3(_currentPositionX, transform.localPosition.y, transform.localPosition.z);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastMousePoint = Input.mousePosition.x;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            lastMousePoint = null;
+        }
+        if (lastMousePoint != null)
+        {
+            difference = Input.mousePosition.x - lastMousePoint.Value;
+            transform.localPosition = new Vector3(Mathf.Clamp(transform.localPosition.x + difference/700, -0.7f, 0.7f), transform.localPosition.y, transform.localPosition.z);
+            lastMousePoint = Input.mousePosition.x;
+        }
+
+        #region Other
+        //transform.localPosition = new Vector3(Mathf.Lerp(transform.localPosition.x, _currentPositionX, Time.deltaTime * _speedSlide), transform.localPosition.y, transform.localPosition.z);
+        //transform.localPosition = Vector3.MoveTowards(transform.localPosition, vectorTarget, Time.deltaTime*_speedSlide);
+        //transform.localPosition = new Vector3(_touchControl.DirectionX, transform.localPosition.y, transform.localPosition.z);
+        #endregion
+    }
+}
